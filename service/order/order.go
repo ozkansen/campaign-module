6e2038eb37d71288eb4f59f3287dbf5596159ffd
Özkan ŞEN <ozkansen@gmail.com>
2023-09-03 03:24:37 +0300

    add order service turnover & average calc

--- service/order/order.go
+++ service/order/order.go
@@ -65,3 +65,33 @@ func (os *OrderService) GetProductTotalSales(productCode string) (int, error) {
 	}
 	return salesTotalQuantity, nil
 }
+
+func (os *OrderService) GetProductTurnOver(productCode string) (int64, error) {
+	orders, err := os.orders.Get(productCode)
+	if err != nil {
+		if errors.Is(err, order.ErrOrdersNotAvailable) {
+			return 0, nil
+		}
+		return 0, err
+	}
+	var turnover int64
+	for _, ord := range orders {
+		turnover += ord.Price
+	}
+	return turnover, nil
+}
+
+func (os *OrderService) GetProductAveragePrice(productCode string) (int64, error) {
+	totalSales, err := os.GetProductTotalSales(productCode)
+	if err != nil {
+		return 0, err
+	}
+	turnOver, err := os.GetProductTurnOver(productCode)
+	if err != nil {
+		return 0, err
+	}
+	if turnOver == 0 || totalSales == 0 {
+		return 0, nil
+	}
+	return turnOver / int64(totalSales), nil
+}
